85218dae54757b8e4fb2cd75e2030a8ad0b69206
Manases Lovera <manaseslovera07@gmail.com>
2026-02-12 17:12:36 -0400

    feat: delete all astro code, use index.html only

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  output: 'static',
-  outDir: './docs',
-  site: 'https://manases.dev', // Replace with your custom domain
-});
